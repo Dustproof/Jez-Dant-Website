d791fc282a29a5ccca434c3a2aad9878c6dd7c94
Jez <jez@dustproof.co>
2020-07-30 07:07:50 +0100

    Nav updates

--- js/scripts.js
+++ js/scripts.js
@@ -15,13 +15,19 @@ navSlide();
 // Nav Scroller
 
 const nav = document.querySelector('nav');
+const navButton = document.getElementById('cv-button')
+const burgerLines = document.querySelector('.burger-line');
 
 window.onscroll = function() {
     var top = window.scrollY;
     console.log(top);
     if (top > 32) {
         nav.classList.add('scrolled');
+        navButton.classList.remove('light');
+        burgerLines.classList.remove('light');
     } else {
         nav.classList.remove('scrolled');
+        navButton.classList.add('light');
+        burgerLines.classList.add('light');
     }
 }
